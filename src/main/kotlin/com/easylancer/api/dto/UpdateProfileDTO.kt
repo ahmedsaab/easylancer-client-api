8c9dcc18189dcbc7ebff765c54f47652cda2a82b
ahmed <ahmed.saab@smaato.com>
2019-10-06 01:08:26 +0200

    fix: null being sent to missing update profile dto

--- src/main/kotlin/com/easylancer/api/dto/UpdateProfileDTO.kt
+++ src/main/kotlin/com/easylancer/api/dto/UpdateProfileDTO.kt
@@ -1,11 +1,14 @@
 package com.easylancer.api.dto
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonInclude
 
 @JsonIgnoreProperties(ignoreUnknown = false)
 data class UpdateProfileDTO(
     val about: String?,
     val languages: Array<String>?,
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     val imagesUrls: Array<String>?,
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     val imageUrl: String?
 )
